ba54ab1d40d7771db239082f827bb689f0677be1
Christian Mack <cmackm@hotmail.com>
2022-03-01 21:24:35 +0100

    Add linter

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <main>
     <div class="container">
+      <Weather />
     </div>
   </main>
 </template>
+
+<script>
+import Weather from './views/Weather.vue'
+
+export default {
+  components: { Weather }
+}
+</script>
 
 <style scoped>
 .container {
---
+++ src/views/Weather.vue
@@ -0,0 +1,14 @@
+<template />
+
+<script>
+const URL = 'https://samples.openweathermap.org/data/2.5/forecast'
+const PARAMS = '?q=M%C3%BCnchen,DE&appid=b6907d289e10d714a6e88b30761fae22'
+
+import axios from 'axios'
+
+export default {
+  setup() {
+
+  },
+}
+</script>
