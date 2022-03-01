bf91b0e57b6c9c094dd4dbb420b60a77193a155e
Christian Mack <cmackm@hotmail.com>
2022-03-01 20:44:10 +0100

    minimal css

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
   <main>
+    <div class="container">
+    </div>
   </main>
 </template>
+
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100%;
+  max-height: 1080px;
+  max-width: 1920px;
+}
+</style>
